52857586e183d1860f068f111b1ef5d55626997c
mandelashaban593 <mandelashaban593@gmail.com>
2021-05-13 02:54:26 +0300

    Added text

---
+++ footer.php
@@ -0,0 +1,84 @@
+<footer>
+<div class="container">
+<div class="row">
+<div class="col-lg-3">
+<div class="widget">
+<h5 class="widgetheading">Our Contact</h5>
+<address>
+<h6>Rural-Urban SACCO ltd has three branches as below;</h6>
+ <h6>   a Juba branch </h6>
+<p>Located in Munuki Suk Libya on the way to Munuki police station.<br/>
+Tel 0922468629/0925789680/0921300516</p>
+   <h6>   b Yei branch </h6>
+<p>Located along Maridi road, next to Nyardene swamp before Yei day s.s road.<br/>
+Tel 0921700760/0924837560</p>
+ <h6>   c Kajo-keji branch </h6>
+<p>Located in Mere to<br/>wn on Juba high way
+Tel 0929354521<br/>
+Email contact: Email.mansuktimon@gmail.com 
+</p>
+</div>
+</div>
+<div class="col-lg-3">
+<div class="widget">
+<h5 class="widgetheading">Quick Links</h5>
+<ul class="link-list">
+<li><a href="about.php">About Us</a></li>
+<li><a href="services.php">Products & Services</a></li>
+<li><a href="portfolio.php">Portfolio</a></li>
+<li><a href="governance.php">Governance</a></li>
+<li><a href="indrecogn.php">Industry recognition</a></li>
+</ul>
+</div>
+</div>
+<div class="col-lg-3">
+<div class="widget">
+<h5 class="widgetheading">Loans</h5>
+<ul class="link-list">
+<li><a href="#">1 - Loans/credit facilities like commercial, school fees and college, agriculture, development, and asset acquisition</a></li>
+<li><a href="#">2 - Savings like personal and group, fixed deposits</a></li>
+<li><a href="#">3 - Money transfer services to all our branches </a></li>
+<li><a href="#">4 - Agency banking with cooperative bank of South Sudan and Equity bank </a></li>
+<li><a href="#">5 - Agricultural loan</a></li>
+<li><a href="#">6 - Development loan for house constractionor for acquiring assets </a></li>
+<li><a href="#">7 - College loan</a></li>
+<li><a href="#">8 - Money transfer</a></li>
+
+</ul>
+</div>
+</div>
+<div class="col-lg-3">
+<div class="widget">
+<h5 class="widgetheading"><!-- Recent News --></h5>
+<ul class="link-list">
+<!-- <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li> -->
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div id="sub-footer">
+<div class="container">
+<div class="row">
+<div class="col-lg-6">
+<div class="copyright">
+<p>
+<span>&copy; Sacco 2021 All right reserved.  </span><a href="#" target="_blank"></a>
+</p>
+</div>
+</div>
+<div class="col-lg-6">
+<ul class="social-network">
+<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</footer>
--- services.php
+++ services.php
@@ -9,6 +9,11 @@
 
 <!-- css -->
 <?php require("style.php"); ?>
+<style type="text/css">
+  img{  width: 180px;
+height:  160px;
+}
+</style>
 
 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
@@ -57,21 +62,23 @@
 <!-- Info Blcoks -->
 <div class="row">
 <div class="col-sm-4 info-blocks">
-<i class="icon-info-blocks fa fa-bell-o"></i>
+<img class="img-responsive" src="img/loans.jpeg" alt="">   
+
 <div class="info-blocks-in">
 <h3><!-- JUNIOR STAR --></h3>
 <p> 1 - Loans/credit facilities like commercial, school fees and college, agriculture, development, and asset acquisition</p>
 </div>
 </div>
 <div class="col-sm-4 info-blocks">
-<i class="icon-info-blocks fa fa-hdd-o"></i>
+<img class="img-responsive" src="img/SavingsAccount.jpg" alt="">   
+
 <div class="info-blocks-in">
 <h3><!-- FontAwesome --></h3>
 <p> 2 - Savings like personal and group, fixed deposits</p>
 </div>
 </div>
 <div class="col-sm-4 info-blocks">
-<i class="icon-info-blocks fa fa-lightbulb-o"></i>
+<img class="img-responsive" src="img/moneytransfer.jpg" alt="">   
 <div class="info-blocks-in">
 <h3><!-- Bootstrap --></h3>
 <p>3 - Money transfer services to all our branches </p>
@@ -84,21 +91,22 @@
 <!-- Info Blcoks -->
 <div class="row">
 <div class="col-sm-4 info-blocks">
-<i class="icon-info-blocks fa fa-code"></i>
+<img class="img-responsive" src="img/AgentBanking.jpg" alt="">
 <div class="info-blocks-in">
 <h3><!-- SEO Ready --></h3>
 <p>4 - Agency banking with cooperative bank of South Sudan and Equity bank</p>
 </div>
 </div>
 <div class="col-sm-4 info-blocks">
-<i class="icon-info-blocks fa fa-compress"></i>
+<img class="img-responsive" src="img/agricLoan.jpg" alt="">
+
 <div class="info-blocks-in">
 <h3><!-- Fully Responsive --></h3>
 <p>5 - Agricultural loan</p>
 </div>
 </div>
 <div class="col-sm-4 info-blocks">
-<i class="icon-info-blocks fa fa-html5"></i>
+<img class="img-responsive" src="img/DevelopmentLoan.jpg" alt="">
 <div class="info-blocks-in">
 <h3><!-- CSS3 + HTML5 --></h3>
 <p>6 - Development loan for house constractionor for acquiring assets</p>
@@ -111,14 +119,14 @@
 <!-- Info Blcoks -->
 <div class="row">
 <div class="col-sm-4 info-blocks">
-<i class="icon-info-blocks fa fa-code"></i>
+<img class="img-responsive" src="img/educationLoan.jpg" alt="">
 <div class="info-blocks-in">
 <h3><!-- SEO Ready --></h3>
 <p> 7 - College loan</p>
 </div>
 </div>
 <div class="col-sm-4 info-blocks">
-<i class="icon-info-blocks fa fa-compress"></i>
+<img class="img-responsive" src="img/moneytransfer.jpg" alt="">
 <div class="info-blocks-in">
 <h3><!-- Fully Responsive --></h3>
 <p> 8 - Money transfer</p>
@@ -130,79 +138,9 @@
 
 </div>
 </section>
-<footer>
-<div class="container">
-<div class="row">
-<div class="col-lg-3">
-<div class="widget">
-<h5 class="widgetheading">Our Contact</h5>
-<address>
-<strong>Target company Inc</strong><br>
-JC Main Road, Near Silnile tower<br>
-Pin-21542 NewYork US.</address>
-<p>
-<i class="icon-phone"></i> (123) 456-789 - 1255-12584 <br>
-<i class="icon-envelope-alt"></i> email@domainname.com
-</p>
-</div>
-</div>
-<div class="col-lg-3">
-<div class="widget">
-<h5 class="widgetheading">Quick Links</h5>
-<ul class="link-list">
-<li><a href="#">Latest Events</a></li>
-<li><a href="#">Terms and conditions</a></li>
-<li><a href="#">Privacy policy</a></li>
-<li><a href="#">Career</a></li>
-<li><a href="#">Contact us</a></li>
-</ul>
-</div>
-</div>
-<div class="col-lg-3">
-<div class="widget">
-<h5 class="widgetheading">Latest posts</h5>
-<ul class="link-list">
-<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
-<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
-<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
-</ul>
-</div>
-</div>
-<div class="col-lg-3">
-<div class="widget">
-<h5 class="widgetheading">Recent News</h5>
-<ul class="link-list">
-<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
-<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
-<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
-</ul>
-</div>
-</div>
-</div>
-</div>
-<div id="sub-footer">
-<div class="container">
-<div class="row">
-<div class="col-lg-6">
-<div class="copyright">
-<p>
-<span>&copy; Site name 2015 All right reserved. Template by </span><a href="http://webthemez.com" target="_blank">WebThemez</a>
-</p>
-</div>
-</div>
-<div class="col-lg-6">
-<ul class="social-network">
-<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
-<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
-</ul>
-</div>
-</div>
-</div>
-</div>
-</footer>
+
+<?php require("footer.php"); ?>
+
 </div>
 <a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
 <!-- javascript
